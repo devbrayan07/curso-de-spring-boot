7c17d3d4f2b9f3ac4a676f12eed02e00b3354348
devbrayan07 <devbrayan007@gmail.com>
2026-02-14 23:09:38 -0300

    first commit

---
+++ src/main/java/com/curso_de_spring_boot/cursospringboot/CursospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.curso_de_spring_boot.cursospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso_de_spring_boot/cursospringboot/CursospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso_de_spring_boot.cursospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
